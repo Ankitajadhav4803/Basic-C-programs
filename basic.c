4f432d9f0c5e1511e30bc2589f5e5a4bfe7a770e
Ankitajadhav4803 <142647278+Ankitajadhav4803@users.noreply.github.com>
2023-08-19 20:37:09 +0530

    Add files via upload

---
+++ basic.c
@@ -0,0 +1,12 @@
+/* accept two numbers from user and perform all arithmetic operations on them */
+int main()
+{
+	int x,y;
+	printf("enter two numbers=");
+	scanf("%d%d", &x, &y);
+	printf("\n addition=%d",x+y);
+	printf("\n substraction=%d",x-y);
+	printf("\n multiplication=%d",x*y);
+	printf("\n division=%d",x/y);
+	printf("\n modular division=%d",x%y);
+}
