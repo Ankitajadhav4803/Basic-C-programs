6f19afd5acdce9f4ed31e8dce4801952a1c8edc4
Ankitajadhav4803 <142647278+Ankitajadhav4803@users.noreply.github.com>
2023-08-20 17:49:31 +0530

    Add files via upload

---
+++ basic1.c
@@ -0,0 +1,14 @@
+/* c program to calculate area and perimeter of a rectangle */
+#include<stdio.h>
+int main()
+{
+	float l,b,area,perimeter;
+	printf("enter length of rectangle:");
+	scanf("%f",&l);
+	printf("enter breadth of rectangle:");
+	scanf("%f",&b);
+	area=l*b;
+	perimeter=2*(l+b);
+	printf("\n area of rectangle=%.2f",area);
+	printf("\n perimeter of rectangle=%.2f",perimeter);
+}
