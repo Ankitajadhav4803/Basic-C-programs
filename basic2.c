360591b6641e49d0f2a902f2e2cd1fed6f5f5f78
Ankitajadhav4803 <142647278+Ankitajadhav4803@users.noreply.github.com>
2023-08-20 17:51:05 +0530

    Add files via upload

---
+++ basic2.c
@@ -0,0 +1,12 @@
+/* c program to calculate area and circumference of a circle */
+#include<stdio.h>
+int main()
+{
+	float r,area,circumference;
+	printf("enter radius of circle:");
+	scanf("%f",&r);
+	area=3.14*r*r;
+	circumference=2*3.14*r;
+	printf("\n area of circle=%.2f",area);
+	printf("\n circumference of circle=%.2f",circumference);
+}
