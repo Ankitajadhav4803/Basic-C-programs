f5d7609a14ecd7fb1cfb750aad6e94fd89e5d9da
Ankitajadhav4803 <142647278+Ankitajadhav4803@users.noreply.github.com>
2023-08-22 18:50:53 +0530

    Add files via upload

---
+++ basic5.c
@@ -0,0 +1,12 @@
+/* c program to calculate surface area and volume of a cylinder */
+#include<stdio.h>
+int main()
+{
+	float r,h,sarea,volume;
+	printf("enter radius,height of a cylinder=");
+	scanf("%f%f",&r,&h);
+	sarea=(2*3.14*r*h)+(2*3.14*r*r);
+	volume=3.14*r*r*h;
+	printf("\n surface area of a cylinder=%.2f",sarea);
+	printf("\n volume of a cylinder=%.2f",volume);
+}
