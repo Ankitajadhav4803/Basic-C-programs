7d1ea7ae025acd2bf5f57c222b6eeeb39be8746c
Ankitajadhav4803 <142647278+Ankitajadhav4803@users.noreply.github.com>
2023-08-23 19:16:59 +0530

    Add files via upload

---
+++ basic6.c
@@ -0,0 +1,12 @@
+/* c program to accept initial velocity(u),acceleration(a),time(t) and print the final velocity(v) and distance travelled(s) */
+#include<stdio.h>
+void main()
+{
+	float u,a,t,v,s;
+	printf("enter initial velocity,acceleration and time=");
+	scanf("%f%f%f",&u,&a,&t);
+	v=u+(a*t);
+	s=u+(a*t*t);
+	printf("\n final velocity=%.2f",v);
+	printf("\n distance travelled=%.2f",s);
+}
