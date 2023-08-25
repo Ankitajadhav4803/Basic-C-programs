115bdafdee3a2ae48cb8953a2cd5f32321d7cc7d
Ankitajadhav4803 <142647278+Ankitajadhav4803@users.noreply.github.com>
2023-08-25 19:55:51 +0530

    Update basic3.c

--- basic3.c
+++ basic3.c
@@ -6,7 +6,7 @@ void main()
 	printf("enter two numbers:");
 	scanf("%f%f",& a,& b);
 	am=(a+b)/2;
-	hm=2*a*b/(a+b);
+	hm=a*b/(a+b);
 	printf("\n arithmetic mean=%.2f",am);
 	printf("\n harmonic mean=%.2f",hm);
 }
