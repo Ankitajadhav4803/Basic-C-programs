27a384c9db2599b4a3d5e06d080d904d3099fb10
Ankitajadhav4803 <142647278+Ankitajadhav4803@users.noreply.github.com>
2023-08-25 17:34:01 +0530

    Add files via upload

---
+++ basic8.c
@@ -0,0 +1,9 @@
+/* c program to accept a character from the user and display its ASCII value */
+#include<stdio.h>
+void main()
+{
+	char ch;
+	printf("enter character=");
+	scanf("%c",&ch);
+	printf("ASCII value of given character=%d",ch);
+}
