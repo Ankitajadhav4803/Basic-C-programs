9f055d3df2d82a75e3ceae1b3b2f00d11a9e52e6
Ankitajadhav4803 <142647278+Ankitajadhav4803@users.noreply.github.com>
2023-08-25 18:59:58 +0530

    Update basic4.c

--- basic4.c
+++ basic4.c
@@ -1,10 +1,12 @@
-/* c program to calculate surface area of a cuboid */
+/* c program to accept three dimensions length (l), breadth (b) and height (h) of a cuboid and print surface area of and volume */
 #include<stdio.h>
 void main()
 {
-	float l,b,h,sarea;
+	float l,b,h,sarea,volume;
 	printf("enter length,breadth,height of a cuboid=");
 	scanf("%f%f%f",&l,&b,&h);
 	sarea=2*((l*b)+(l*h)+(b*h));
+ volume=l*b*h;
 	printf("\n surface area of a cuboid=%.2f",sarea);
+ printf("\n volume of a cuboid=%.2f", volume);
 }
