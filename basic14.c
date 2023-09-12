09f0b56820f67149c43ee70deb8317d21d2be98b
Ankitajadhav4803 <142647278+Ankitajadhav4803@users.noreply.github.com>
2023-09-12 19:15:01 +0530

    Add files via upload

---
+++ basic14.c
@@ -0,0 +1,12 @@
+/* c program to accept marks for three subjects and find the total marks secured, average */
+#include<stdio.h>
+void main()
+{
+	float m1,m2,m3,avg,tot;
+	printf("enter marks for three subjects=");
+	scanf("%f%f%f",&m1,&m2,&m3);
+	tot=m1+m2+m3;
+	avg=tot/3;
+	printf("\n total marks secured=%.2f",tot);
+	printf("\n average=%.2f",avg);
+} 
